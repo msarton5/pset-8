b4790250558291afc5eecf7b7cc0ca9d6af6239d
Molly Sarton <msarton@ucvts.org>
2020-02-02 16:04:19 -0500

    commit 8

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@ let board;
 let turn;
 let win;
 
-// ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-// const squares = Array.from(document.querySelectorAll("#board div"));
-// const message = document.querySelector("h2");
-//
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+
 // ///////////////////// EVENT LISTENERS ///////////////////////////////
 // window.onload = init;
 // document.getElementById("board").onclick = takeTurn;
