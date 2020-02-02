15ced2e94d300452c766c2c3c24b2a6fcb00236b
Molly Sarton <msarton@ucvts.org>
2020-02-02 16:05:07 -0500

    commit 9?

--- js/app.js
+++ js/app.js
@@ -19,10 +19,10 @@ let win;
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
 
-// ///////////////////// EVENT LISTENERS ///////////////////////////////
-// window.onload = init;
-// document.getElementById("board").onclick = takeTurn;
-//
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+
 // ///////////////////// FUNCTIONS /////////////////////////////////////
 // function init() {
 //   board = [
